fcd1ad51c1de804fce23b6967a1ae97fffe1fde4
Zorca Orcinus <vs@zorca.org>
2019-11-28 21:41:35 +0300

    Init

---
+++ widgetizer.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * Plugin Name: Widgetizer
+ * Plugin URI:  https://github.com/wpseed/widgetizer
+ * Description: Widgets Generator for Page Builders.
+ * Version:     1.0.0
+ * Author:      WP Seed
+ * Author URI:  https://wpseed.io/
+ * Donate link: https://wpseed.io/donate/
+ * License:     GPLv2
+ *
+ * Text Domain: widgetizer
+ * Domain Path: /languages
+ *
+ * @link    https://wpseed.io/
+ *
+ * @package Widgetizer
+ * @version 1.0.0
+ */
+
+/**
+ * Currently plugin version.
+ * Starts at version 1.0.0 and uses SemVer - https://semver.org
+ */
+define( 'WPSEED_WIDGETIZER_VERSION', '1.0.0' );
+/**
+ * Minimum required php version.
+ */
+define( 'WPSEED_WIDGETIZER_MIN_PHP_VERSION', '5.6.20' );
+/**
+ * Path to the plugin dir.
+ */
+define( 'WPSEED_WIDGETIZER_PATH', __DIR__ );
+/**
+ * Basepath of the plugin.
+ */
+define( 'WPSEED_WIDGETIZER_BASENAME', plugin_basename( __FILE__ ) );
+/**
+ * Url of the plugin dir.
+ */
+define( 'WPSEED_WIDGETIZER_URL', plugin_dir_url( __FILE__ ) );
