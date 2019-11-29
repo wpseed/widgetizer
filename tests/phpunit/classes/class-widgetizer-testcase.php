2031716550cc5879360e22754600ecb69d86e0f2
Zorca Orcinus <vs@zorca.org>
2019-11-29 22:00:04 +0300

    TestCase class added

---
+++ tests/phpunit/classes/class-widgetizer-testcase.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Wigdetizer_TestCase class file.
+ *
+ * @package Widgetizer
+ */
+
+use PHPUnit\Framework\TestCase;
+use tad\FunctionMocker\FunctionMocker;
+
+/**
+ * Class Widgetizer_TestCase
+ */
+abstract class Widgetizer_TestCase extends TestCase {
+
+}
