fd1b06b6a2ad404826c1f52f75b0b066d7cf8c08
Zorca Orcinus <vs@zorca.org>
2019-12-29 21:04:04 +0300

    Rebuild columns

--- assets-src/scripts/components/AllWidgetsComponent.vue
+++ assets-src/scripts/components/AllWidgetsComponent.vue
@@ -2,20 +2,28 @@
   <section>
     <b-table
       :data="widgets"
-      :columns="columns"
       :paginated=true
       :per-page=10
       :pagination-simple=false>
       <template slot-scope="props">
-        <b-table-column field="widget_name" label="Widget Name" sortable>
+        <b-table-column field="widget_name" label="Widget Name" sortable searchable>
           <router-link :to="'/widgets/' + props.row.widget_provider + '/' + props.row.widget_name">
-            {{props.row.widget_provider }}/{{ props.row.widget_name }}
+            {{ props.row.widget_name }}
           </router-link>
         </b-table-column>
-        <b-table-column field="widget_actions" width="300">
+        <b-table-column field="widget_provider" label="Widget Provider" sortable searchable>
+          {{ props.row.widget_provider }}
+        </b-table-column>
+        <b-table-column field="widget_actions" label="" width="220">
           <div class="buttons">
-            <b-button type="is-success" icon-left="content-copy">Duplicate</b-button>
-            <b-button type="is-danger" icon-left="delete">Delete</b-button>
+            <b-button type="is-success" icon-left="content-copy"
+              v-on:click="duplicateWidget(props.row.widget_provider, props.row.widget_name)">
+              Duplicate
+            </b-button>
+            <b-button type="is-danger" icon-left="delete" v-if="props.row.widget_provider !== 'widgetizer'"
+              v-on:click="deleteWidget(props.index, props.row.widget_provider, props.row.widget_name)">
+              Delete
+            </b-button>
           </div>
         </b-table-column>
       </template>
@@ -28,38 +36,58 @@ export default {
   data() {
     return {
       widgets: [],
-      columns: [
-        {
-          field: 'widget_name',
-          label: 'Widget Name',
-        },
-        {
-          field: 'widget_icon',
-          label: 'Icon',
-        },
-        {
-          field: 'widget_path',
-          label: 'Widget Path',
-        },
-      ],
       errors: [],
-      dataWpseedWidgetizerAdmin: window.dataWpseedWidgetizerAdmin
-    }
+      dataWpseedWidgetizerAdmin: window.dataWpseedWidgetizerAdmin,
+    };
+  },
+  methods: {
+    duplicateWidget(provider, name) {
+      console.log([provider, name]);
+    },
+    deleteWidget(index, provider, name) {
+      this.$buefy.dialog.confirm({
+        title: 'Deleting widget',
+        message: 'Are you sure you want to <b>delete</b> widget? This action cannot be undone.',
+        confirmText: 'Delete Widget',
+        type: 'is-danger',
+        hasIcon: true,
+        onConfirm: () => {
+          window.axios
+            .delete(`${dataWpseedWidgetizerAdmin.restUrl}widgetizer/v1/widgets/${provider}/${name}`)
+            .then(
+              (response) => {
+                this.$data.widgets.splice(index, 1);
+                this.$buefy.toast.open({
+                  message: 'Widget deleted',
+                  type: 'is-success',
+                });
+                console.log(index);
+              },
+            )
+            .catch(
+              (e) => {
+                this.errors.push(e);
+              },
+            );
+          console.log([provider, name]);
+        }
+      });
+    },
   },
   created() {
     window.axios
-      .get(dataWpseedWidgetizerAdmin.restUrl + 'widgetizer/v1/widgets/')
+      .get(`${dataWpseedWidgetizerAdmin.restUrl}widgetizer/v1/widgets/`)
       .then(
-        response => {
+        (response) => {
           this.widgets = response.data;
           console.log(this.widgets);
-        }
-      )
-      .catch(
-        e => {
-          this.errors.push(e)
         },
       )
-  }
-}
+      .catch(
+        (e) => {
+          this.errors.push(e);
+        },
+      );
+  },
+};
 </script>
